d835ff58ca8f5ed3160cbf2a97cd8d374aec128f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:45:36 -0500

    tunit

--- test/Soenneker.GitHub.OpenApiClient.Tests/GitHubOpenApiClientTests.cs
+++ test/Soenneker.GitHub.OpenApiClient.Tests/GitHubOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.GitHub.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public class GitHubOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class GitHubOpenApiClientTests : HostedUnitTest
 {
-    public GitHubOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public GitHubOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
