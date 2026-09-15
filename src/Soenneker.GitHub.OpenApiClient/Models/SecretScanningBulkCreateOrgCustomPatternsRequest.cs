2551cc7c4d2504a401afcd3261fbf57c6a8cbf87
Jake Soenneker <jake@soenneker.com>
2026-09-15 20:04:29 +0000

    Automated update

--- src/Soenneker.GitHub.OpenApiClient/Models/SecretScanningBulkCreateOrgCustomPatternsRequest.cs
+++ src/Soenneker.GitHub.OpenApiClient/Models/SecretScanningBulkCreateOrgCustomPatternsRequest.cs
@@ -14,7 +14,7 @@ namespace Soenneker.GitHub.OpenApiClient.Models
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The list of custom patterns to create.</summary>
+        /// <summary>The list of custom patterns to create (maximum 100).</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
         public List<global::Soenneker.GitHub.OpenApiClient.Models.SecretScanningCustomPatternToCreate>? Patterns { get; set; }
